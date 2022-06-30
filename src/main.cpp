6e2d220a62557832bf5c077d0a6d7333976c7458
Jesus Flores <me@jesusfloressanjose.com>
2022-06-30 18:36:00 +0200

    Thread sum of numbers

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,30 @@
+#include <boost/thread.hpp>
 #include <iostream>
+#include <cstdint>
 
-int main() {
-	std::cout << "Hello, World!" << std::endl;
-	return 0;
+uint64_t total = 0;
+
+void calculateSum(int maxNumber, int start = 0)
+{
+	std::uint64_t localTotal = 0;
+	int i = start;
+	for (i; i <= maxNumber; i++) {
+		localTotal += i;
+	}
+
+	total += localTotal;
+}
+
+int main()
+{
+	int number = 1'000'000'000;
+
+	double cutPoint = number / 2.0;
+	int lowerHalf = floor(cutPoint);
+
+	calculateSum(lowerHalf, 0);
+	boost::thread t1{calculateSum, number, lowerHalf + 1};
+	t1.join();
+
+	std::cout << "sum is " << total << std::endl;
 }
